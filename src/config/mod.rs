5fa89979be8b3f8fd312b7045cadc3de17b51701
Pedro Klein <pedroklein1@hotmail.com>
2026-02-17 19:24:11 -0300

    Extend config schema for registry settings

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5,5 +5,6 @@ mod schema;
 
 pub use loader::{load_dag_config, load_dag_config_unchecked};
 pub use schema::{
-    DagConfig, EdgeDefinition, NodeDefinition, NodeType, DEFAULT_FUEL_LIMIT, DEFAULT_QUEUE_CAPACITY,
+    DagConfig, EdgeDefinition, NodeConfig, NodeDefinition, NodeType, DEFAULT_FUEL_LIMIT,
+    DEFAULT_QUEUE_CAPACITY,
 };
